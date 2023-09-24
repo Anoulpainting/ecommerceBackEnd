70a734619ae2fa901c1252bd720eee42c17952a8
Anoulpainting <142727527+Anoulpainting@users.noreply.github.com>
2023-09-24 13:37:44 +0200

    formmulti

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -32,6 +32,22 @@ export default function ProductForm({
     if (goToProducts) {
         router.push('/products');
     }
+    async function uploadImages(ev) {
+        const files = ev.target.files;
+        if (files.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+
+                data.append('file', file);
+            }
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data,
+            })
+            console.log(res);
+        }
+
+    }
     return (
 
         <form onSubmit={saveProduct}>
@@ -46,11 +62,12 @@ export default function ProductForm({
 
             </label>
             <div className="mb-2">
-                <button className="btn-up" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <label className="btn-up" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
                 </svg>
                     <div > Téléverser</div>
-                </button>
+                    <input type="file" className="hidden" onChange={uploadImages} />
+                </label>
 
                 {!images?.length && (
                     <div> <h2><b>Pas d'images pour ce produit</b></h2> </div>
@@ -70,7 +87,8 @@ export default function ProductForm({
                 placeholder="Prix"
                 value={price}
                 onChange={ev => setPrice(ev.target.value)} />
-            <button className="btn-primary" type="submit">Enregistrer</button>
+            <button className="btn-primary" type="submit">Enregistrer
+            </button>
         </form>
 
     );
